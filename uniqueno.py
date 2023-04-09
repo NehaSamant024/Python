78f7bdb390b987306fcb5f41127c162635edfb49
Neha Samant <91238040+NehaSamant024@users.noreply.github.com>
2023-04-09 12:11:52 +0530

    Add files via upload

---
+++ uniqueno.py
@@ -0,0 +1,11 @@
+num1= int(input("enter the first no"))
+num2= int(input("enter the second no"))
+num3= int(input("enter the third no"))
+num4= int(input("enter the fourth no"))
+num5= int(input("enter the fifth no"))
+num6= int(input("enter the sixth no"))
+num7= int(input("enter the seventh no"))
+num8= int(input("enter the eight no"))
+
+s={num1,num2,num3,num4,num5,num6,num7,num8}
+print(s)
