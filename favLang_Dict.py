416d34d813c97a0035bc269a491168b9ab21e3d0
Neha Samant <91238040+NehaSamant024@users.noreply.github.com>
2023-04-09 12:31:39 +0530

    Add files via upload

---
+++ favLang_Dict.py
@@ -0,0 +1,11 @@
+'''Create an empty dictionary . Allow 4 friends to enter their favourite language as values & use keys as their names. Assume that the names are unique'''
+favLang={}
+a=input("fav lang of neha")
+b=input("fav lang of raja")
+c=input("fav lang of riya")
+d=input("fav lang of yogi")
+favLang["neha"]=a
+favLang["raja"]=b
+favLang["riya"]=c
+favLang["yogi"]=d
+print(favLang)
